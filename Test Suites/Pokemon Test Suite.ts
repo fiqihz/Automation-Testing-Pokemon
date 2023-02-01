597891960533b1b1062b7278aa338d50a8bacf89
Andi Fiqih Zulfikar Ashari <fiqihz096@gmail.com>
2023-02-01 23:52:38 +0700

    Adding screen recorder when running execution & add Test Suite

---
+++ Test Suites/Pokemon Test Suite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Pokemon Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>263daa5d-b1f6-4864-881f-e3d24d8e7905</testSuiteGuid>
+   <testCaseLink>
+      <guid>1b10dca2-c414-4eee-a3e0-31fea6e5a6ce</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC001_SearchPokemon</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
